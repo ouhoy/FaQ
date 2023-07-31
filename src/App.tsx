a2c9a9e9c71b4824e376da0185ffcd57f48cf593
ouhoy <kimkinon@gmail.com>
2023-07-31 09:52:24 +0100

    Show and hide answers onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {faqs} from "./homePageData.ts";
 import "./App.css"
+import {useState} from "react";
 
 function App() {
 
@@ -28,15 +29,21 @@ function Faq({faqs}: { faqs: { question: string, answer: string }[] }) {
 }
 
 function FaqItem({question, answer}: { question: string, answer: string }) {
+    const [showQuestion, setShowQuestion] = useState(false);
+
+    function handleToggle() {
+        setShowQuestion(!showQuestion)
+    }
+
     return (<div className="question-border-wrap">
-        <div className="question-container">
+        <div className={`question-container ${showQuestion ? "show-question" : ""}`} onClick={handleToggle}>
             <div className="question">
                 <p>{question}</p>
                 <div className="plus">
                     <div className="line"></div>
                     <div
                         className="line plus-line"
-                        style={{"transform": "rotate(180deg)"}}
+                        style={{"transform": `${showQuestion ? "rotate(90deg)" : "rotate(180deg)"}`}}
                     ></div>
                 </div>
             </div>
